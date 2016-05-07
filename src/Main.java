95310f43123fa89b9b270c74092e0d7da898f312
Pieter <pietervanderveeken@gmail.com>
2016-05-07 14:11:22 +0200

    Forgot to modify Main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main
 	public static void main(String[] args)
 	{
 		MessageCommunicator msgCom = new MessageCommunicator();
-		msgCom.sendMessage("testtesttest");
+		msgCom.sendMessage("TIM", "testtesttest");
 		System.out.println(msgCom.receiveMessage());
 	}
 }
