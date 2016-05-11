605468e0f565401c516e1d168f93a81cea71b967
Pieter <pietervanderveeken@gmail.com>
2016-05-11 16:39:41 +0200

    kut ajax

--- src/Config.java
+++ src/Config.java
@@ -9,7 +9,7 @@ class Config
 	static final String   keyFileExtension = ".pem";
 	static final String   AESMode          = "AES/CBC/PKCS7Padding";
 	static final String   RSAMode          = "RSA/None/OAEPWithSHA1AndMGF1Padding";
-	static final String   AESIV            = "AFCAJAX1KAMPIOEN";
+	static final String   AESIV            = "GODVERDOMMEAJAX1";
 	/* Mixnet Settings */
 	static final String[] nodes            = new String[]{"A", "B", "C", "Cache"};
 	/* Log Settings */
